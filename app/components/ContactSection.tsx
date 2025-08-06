d0b17904eb67a52b293082b6e190526a0b24e534
Next-Level-Dev <shadowdiary01@gmail.com>
2025-08-06 17:43:09 +0300

    Update ContactSection.tsx

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -54,7 +54,7 @@ export default function ContactSection() {
 								<svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
 									<path d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" />
 								</svg>
-								<span>Download Resume [not done]</span>
+								<span>I AM NOT LEAKING MY RESUME</span>
 							</motion.a>
 							<p className="text-green-500 mt-8 mb-4">$ ls ./social-links</p>
 							<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -69,8 +69,8 @@ export default function ContactSection() {
 										</svg>
 									</div>
 									<div>
-										<p className="font-semibold text-gray-200">GitHub</p>
-										<p className="text-sm text-gray-400">@[private account]</p>
+										<p className="font-semibold text-gray-200">Roblox</p>
+										<p className="text-sm text-gray-400">NextLevel_Dev</p>
 									</div>
 								</motion.a>
 								<motion.a
@@ -85,7 +85,7 @@ export default function ContactSection() {
 									</div>
 									<div>
 										<p className="font-semibold text-gray-200">LinkedIn</p>
-										<p className="text-sm text-gray-400">[Fullname Cencored]</p>
+										<p className="text-sm text-gray-400">[IM NOT LEAKING MY NAME TODAY]</p>
 									</div>
 								</motion.a>
 								<motion.a
@@ -99,8 +99,8 @@ export default function ContactSection() {
 										</svg>
 									</div>
 									<div>
-										<p className="font-semibold text-gray-200">X</p>
-										<p className="text-sm text-gray-400">@[no account]</p>
+										<p className="font-semibold text-gray-200">Discord</p>
+										<p className="text-sm text-gray-400">.dinker.</p>
 									</div>
 								</motion.a>
 							</div>
