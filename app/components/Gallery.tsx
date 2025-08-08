b7f8f991f82efd4d2959fd415856101392ed8754
Next-Level-Dev <shadowdiary01@gmail.com>
2025-08-08 18:26:27 +0300

    image change

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 import React from 'react';
 
 const images = [
-  'https://source.unsplash.com/random/800x600?sig=1',
-  'https://source.unsplash.com/random/800x600?sig=2',
-  'https://source.unsplash.com/random/800x600?sig=3',
-  'https://source.unsplash.com/random/800x600?sig=4',
-  'https://source.unsplash.com/random/800x600?sig=5',
+  'https://i.postimg.cc/YS9KPR8N/Untitled-design.png',
+  'https://i.postimg.cc/YS9KPR8N/Untitled-design.png',
+  'https://i.postimg.cc/YS9KPR8N/Untitled-design.png',
+  'https://i.postimg.cc/YS9KPR8N/Untitled-design.png',
+  'https://i.postimg.cc/YS9KPR8N/Untitled-design.png',
 ];
 
 export default function Gallery() {
