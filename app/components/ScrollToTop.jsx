75f3a0048544dd03e0e53f790a5377a5d93bba90
Next-Level-Dev <shadowdiary01@gmail.com>
2025-08-08 20:31:08 +0300

    MORE DELAY

--- app/components/ScrollToTop.jsx
+++ app/components/ScrollToTop.jsx
@@ -9,7 +9,7 @@ export default function ScrollToTop() {
     // Use a short timeout to run after browser's default scroll restore
     const timeout = setTimeout(() => {
       window.scrollTo(0, 0);
-    }, 50); // 50ms delay, you can tweak this if needed
+    }, 250); // 50ms delay, you can tweak this if needed
 
     return () => clearTimeout(timeout);
   }, [pathname]);
