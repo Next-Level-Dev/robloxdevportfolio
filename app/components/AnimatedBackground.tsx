47eb20323bf3db1e70797827e1c5f9a838fb43f6
Next-Level-Dev <shadowdiary01@gmail.com>
2025-08-09 14:11:31 +0300

    diag change

--- app/components/AnimatedBackground.tsx
+++ app/components/AnimatedBackground.tsx
@@ -6,7 +6,7 @@ export default function AnimatedBackground() {
   return (
     <motion.div
       className="absolute inset-0 bg-[url('/grid2.svg')] bg-repeat opacity-40"
-      animate={{ backgroundPosition: ["0 0", "100px 100px"] }}
+      animate={{ backgroundPosition: ["0 0", "-100px 100px"] }}
       transition={{ duration: 6, repeat: Infinity, ease: "linear" }}
     />
   );
