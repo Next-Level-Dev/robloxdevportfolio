139cf78c9dee63d6862aba0d286e0d250a0ed7ff
Next-Level-Dev <shadowdiary01@gmail.com>
2025-08-09 13:24:24 +0300

    error fix

--- app/components/PageWrapper.tsx
+++ app/components/PageWrapper.tsx
@@ -1,27 +1,24 @@
-use client;
+"use client";
 
-import { AnimatePresence, motion } from framer-motion;
-import { usePathname } from nextnavigation;
+import { ReactNode } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
-export default function PageWrapper({
-  children,
-} {
-  children React.ReactNode;
-}) {
+export default function PageWrapper({ children }: { children: ReactNode }) {
   const pathname = usePathname();
 
   return (
-    AnimatePresence mode=wait
-      motion.div
+    <AnimatePresence mode="wait">
+      <motion.div
         key={pathname}
-        initial={{ opacity 0, y 10 }}
-        animate={{ opacity 1, y 0 }}
-        exit={{ opacity 0, y -10 }}
-        transition={{ duration 0.3, ease easeInOut }}
-        className=animated-text
-      
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.4, ease: "easeInOut" }}
+        className="animated-text"
+      >
         {children}
-      motion.div
-    AnimatePresence
+      </motion.div>
+    </AnimatePresence>
   );
 }
