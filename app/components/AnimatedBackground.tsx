19c3970ac96217943784ebbfb4b22025dbd85a18
Next-Level-Dev <shadowdiary01@gmail.com>
2025-08-09 14:10:17 +0300

    10x speed

--- app/components/AnimatedBackground.tsx
+++ app/components/AnimatedBackground.tsx
@@ -7,7 +7,7 @@ export default function AnimatedBackground() {
     <motion.div
       className="absolute inset-0 bg-[url('/grid2.svg')] bg-repeat opacity-40"
       animate={{ backgroundPosition: ["0 0", "100px 100px"] }}
-      transition={{ duration: 60, repeat: Infinity, ease: "linear" }}
+      transition={{ duration: 6, repeat: Infinity, ease: "linear" }}
     />
   );
 }
