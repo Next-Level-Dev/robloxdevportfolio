8374ae591c1c80301ee6b29fb1be5b5337fa9ffc
Next-Level-Dev <shadowdiary01@gmail.com>
2025-08-09 14:08:12 +0300

    declare

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { ReactNode } from "react";
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import HashCleaner from './components/HashCleaner';
